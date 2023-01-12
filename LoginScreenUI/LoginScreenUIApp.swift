9fcdb7aab750abcd4dc8b5e4ad9b13d49b48bc48
snchxlr <sean.cheah@liveramp.com>
2023-01-12 11:39:36 +0800

    Initial Commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by scheah on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
