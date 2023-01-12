d45e0be79f5dee04213a8dfc4b55b060a30b17e1
snchxlr <sean.cheah@liveramp.com>
2023-01-12 16:10:02 +0800

    first

--- LoginScreenUI/ContentView.swift
+++ LoginScreenUI/ContentView.swift
@@ -6,16 +6,81 @@
 //
 
 import SwiftUI
+import LRAtsSDK
+
+let appId = "e47b5b24-f041-4b9f-9467-4744df409e31"
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var wrongEmail: Float = 0
+    @State private var wrongPassword: Float  = 0
+    @State private var showingLoginScreen = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Email", text: $email)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongEmail))
+                        
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateEmail(email: email, password: password)
+                        }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(email)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }.navigationBarHidden(true)
         }
-        .padding()
+    }
+    
+
+    
+    func authenticateEmail(email: String, password: String) {
+        if email.lowercased() == "admin@gmail.com" {
+            wrongEmail = 0
+            if password.lowercased() == "admin" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongEmail = 2
+        }
     }
 }
 
@@ -24,3 +89,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- LoginScreenUI/LoginScreenUIApp.swift
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import LRAtsSDK
 
 @main
 struct LoginScreenUIApp: App {
